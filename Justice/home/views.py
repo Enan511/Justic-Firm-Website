0d112e0be860d288adde72170eed0866e67606e5
enanalharun@gmail.com <enanalharun@gmail.com>
2024-07-17 00:40:11 +0600

    First Commit

---
+++ Justice/home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+def home2(request):
+    return render(request, 'index-2.html')
+def home3(request):
+    return render(request, 'index-3.html')
+def home4(request):
+    return render(request, 'index-4.html')
